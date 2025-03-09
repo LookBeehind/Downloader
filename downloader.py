1e67b6244505667e2661ae63c68272e353933c7b
exedis <lifiu03@gmail.com>
2025-03-09 12:17:34 +0200

    a

--- downloader.py
+++ downloader.py
@@ -1,6 +1,5 @@
-import os
-from pytube.cli import on_progress
 import yt_dlp
+
 
 def on_progress(d):
     if d['status'] == 'finished':
@@ -8,16 +7,20 @@ def on_progress(d):
 
 
 def download(url, path, video_resolution=1080, ext='Default', form='Video + Audio'):
-
     ffmpeg_loc = 'C:/ffmpeg/bin'
+    # Common options across all formats
+    common_opts = {
+        'outtmpl': f'{path}/%(title)s.%(ext)s',
+        'max_retries': 5,
+        'download_archive': 'downloaded.txt',
+        'progress_hooks': [on_progress],
+    }
+
     if form == 'Video + Audio':
         ydl_opts = {
             'format': f'bestvideo[height<={video_resolution}]+bestaudio/best[height<={video_resolution}]',
-            'outtmpl': f'{path}/%(title)s.%(ext)s',
             'ffmpeg_location': ffmpeg_loc,
-            'max_retries': 5,
         }
-
         if ext != 'Default':
             ydl_opts['postprocessors'] = [{
                 'key': 'FFmpegVideoConvertor',
@@ -26,30 +29,24 @@ def download(url, path, video_resolution=1080, ext='Default', form='Video + Audi
     elif form == 'Video':
         ydl_opts = {
             'format': f'bestvideo[height<={video_resolution}]',
-            'outtmpl': f'{path}/%(title)s.%(ext)s',
-            'max_retries': 5,
         }
-    else:
+    else:  # Audio only
         ydl_opts = {
-            'format': f'bestaudio/best',
-            'outtmpl': f'{path}/%(title)s.%(ext)s',
+            'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
             }],
             'ffmpeg_location': ffmpeg_loc,
-            'max_retries': 5,
         }
+
+    ydl_opts.update(common_opts)
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=False, process=False)
-
         if 'entries' in info_dict:
             for entry in info_dict['entries']:
                 ydl.download([entry['url']])
         else:
             ydl.download([url])
-
-
-# download(url='https://www.youtube.com/watch?v=8V_B0hgDKU0=1', form='Video', video_resolution=2160)
